3ed85b8066e2e181c2f7c48678581d802cdc5f3c
Lampt <coldwind1911@gmail.com>
2018-08-16 11:44:15 +0700

    Create migration

---
+++ database/migrations/2018_08_16_034021_create_specializes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSpecializesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('specializes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('teaching_grade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('specializes');
+    }
+}
